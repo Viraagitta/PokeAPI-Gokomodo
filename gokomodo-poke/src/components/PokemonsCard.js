d2f045d0d5a6ee40a7163384e2a0bc507bf6821c
Viraagitta <viraagitta99@gmail.com>
2022-09-06 09:05:07 +0700

    feat: pokemon card list

--- gokomodo-poke/src/components/PokemonsCard.js
+++ gokomodo-poke/src/components/PokemonsCard.js
@@ -1,22 +1,57 @@
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-
-function PokemonsCard({ poke }) {
+import { useDispatch, useSelector } from "react-redux";
+import { fetchDetailPokemon } from "../store/actions";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Card } from "flowbite-react";
+const Sprite = styled.img`
+  width: 300px;
+  height: 300px;
+  alignself: center;
+  margin: auto;
+  padding: 10px;
+  display: flex;
+  flex-wrap: wrap;
+`;
+function PokemonsCard({ poke, i }) {
+  // console.log(poke, "<<card");
   const dispatch = useDispatch();
 
-  const navigate = useNavigate();
+  const detailPokemon = useSelector((state) => state.detailPokemon);
 
+  useEffect(() => {
+    dispatch(fetchDetailPokemon(poke.name));
+  }, []);
   // const detailHandler = (id) => {
   //   // console.log(id);
   //   navigate(`/${id}`);
   // };
 
   return (
-    <>
-      <div>POKEMON CARD {poke.name}</div>
-    </>
+    <div className="flex flex-row">
+      <Card>
+        <div class="rounded overflow-hidden shadow-lg">
+          <Sprite
+            className="flex items-center"
+            src={poke.sprites.other["official-artwork"]["front_default"]}
+          />
+          <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">{poke.name}</div>
+          </div>
+          <div class="px-6 pt-4 pb-2">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #photography
+            </span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #travel
+            </span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #winter
+            </span>
+          </div>
+        </div>
+      </Card>
+    </div>
   );
 }
 
--- gokomodo-poke/src/pages/HomePage.js
+++ gokomodo-poke/src/pages/HomePage.js
@@ -2,25 +2,55 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import PokemonsCard from "../components/PokemonsCard";
-import { fetchPokemons } from "../store/actions";
+import { fetchDetailPokemon, fetchPokemons } from "../store/actions";
 
 export default function HomePage() {
-  const dispatch = useDispatch();
-  const pokemons = useSelector((state) => state.pokemons);
+  // const dispatch = useDispatch();
+  // let pokemons = useSelector((state) => state.pokemons);
+  // const detailPokemon = useSelector((state) => state.detailPokemon);
+
+  // const [loading, setLoading] = useState(true);
+  // useEffect(() => {
+  //   setLoading(false);
+  //   dispatch(fetchPokemons());
+  // }, []);
+
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [loadMore, setLoadMore] = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=20"
+  );
+
+  const getAllPokemons = async () => {
+    const res = await fetch(loadMore);
+    const data = await res.json();
+
+    setLoadMore(data.next);
+
+    function createPokemonObject(results) {
+      results.forEach(async (pokemon) => {
+        const res = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        );
+        const data = await res.json();
+        setAllPokemons((currentList) => [...currentList, data]);
+        await allPokemons.sort((a, b) => a.id - b.id);
+      });
+    }
+    createPokemonObject(data.results);
+  };
 
   useEffect(() => {
-    dispatch(fetchPokemons());
-  });
+    getAllPokemons();
+  }, []);
 
   return (
     <>
       <div className="container-fluid">
         <div className="row">
           <div className="col py-3">
-            {/* <h3></h3> */}
-            <div className="content-container">
-              {pokemons.results?.map((poke) => {
-                return <PokemonsCard key={poke.id} poke={poke} />;
+            <div className="flex flex-wrap justify-center gap-4">
+              {allPokemons.map((poke, i) => {
+                return <PokemonsCard key={poke.id} poke={poke} i={i} />;
               })}
             </div>
           </div>
